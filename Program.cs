bcf690476429a59f4bad8ae54cdfc34ee0ea5751
AndyK <bolt999@yandex.ru>
2021-11-10 03:40:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Kuklin Anrey Vladimirovich";
+            int age = 49;
+            string e_mail = "andykook@yandex.ru";
+            double progr = 3.52D;
+            float maths = 4.36F;
+            decimal physics = 5.25M;
+
+            string ageFormat = Convert.ToString(age);
+            string progrFormat = progr.ToString();
+            string mathFormat = maths.ToString();
+            string physicsFormat = physics.ToString();
+
+            string output = $" FullName : {fullName}. age : {ageFormat}. e-mail : {e_mail}.\n " +
+                $"Scores : programming : {progrFormat}. mathematics : {mathFormat}. physics : {physicsFormat}";
+
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}
